20497043a9d8ca0d33ad233ce6e6521be5f6c1c3
Kurt Corbett <kurtcorbett@gmail.com>
2015-04-08 19:54:33 -0600

    add gulpfile with browserify, watch, serve tasks

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var browserify  = require('browserify')
+  , connect     = require('gulp-connect')
+  , gulp        = require('gulp')
+  , source      = require('vinyl-source-stream');
+
+gulp.task('browserify', function() {
+  return browserify('./client/app/app.module.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./client/app'));
+});
+
+gulp.task('build', ['browserify']);
+
+gulp.task('default', ['watch','serve']);
+
+gulp.task('serve', function() {
+  connect.server({
+    port: 8080,
+    hostname: 'localhost',
+    root: './client/app'
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./client/app/**/*.js', ['browserify']);
+});
