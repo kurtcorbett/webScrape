4c07d13999cbe9c36e4fb622cb1e6e86c9ed7af8
Chris Lovato <christopher.lovato@vivint.com>
2015-04-14 22:13:34 -0600

    change classifieds routes

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -1,41 +1,26 @@
-var angular = require('angular')
+/* @ngInject */
+function configure($stateProvider, $urlRouterProvider) {
 
-(function() {
-    'use strict';
+  $urlRouterProvider.otherwise("/");
+//
+// Now set up the states
+  $stateProvider
+    .state('classifieds', {
+      url: "/",
+      templateUrl: "./classifieds/classifieds.html",
+    })
+    .state('state2', {
+      url: "/state2",
+      templateUrl: "./layout/state2.html"
+    })
+    .state('state2.list', {
+      url: "/list",
+      templateUrl: "./layout/state2.list.html",
+      controller: function($scope) {
+        $scope.things = ["A", "Set", "Of", "Things"];
+      }
+    });
 
-    angular
-        .module('app')
-        .config(configure)
+}
 
-    function configure($stateProvider, $urlRouterProvider) {
-
-      $urlRouterProvider.otherwise("/state1");
-  //
-  // Now set up the states
-      $stateProvider
-        .state('state1', {
-          url: "/",
-          templateUrl: "./layout/state1.html"
-        })
-        .state('state1.list', {
-          url: "/list",
-          templateUrl: "./layout/state1.list.html",
-          controller: function($scope) {
-            $scope.items = ["A", "List", "Of", "Items"];
-          }
-        })
-        .state('state2', {
-          url: "/state2",
-          templateUrl: "./layout/state2.html"
-        })
-        .state('state2.list', {
-          url: "/list",
-          templateUrl: "./layout/state2.list.html",
-          controller: function($scope) {
-            $scope.things = ["A", "Set", "Of", "Things"];
-          }
-        });
-
-    }
-
-})();
+module.exports  = configure;
