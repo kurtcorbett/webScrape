196bf3c40d66040a3cbfdf2dae5a9d6b23891feb
Chris Lovato <christopher.lovato@vivint.com>
2015-04-14 22:19:33 -0600

    add classifieds html template

--- client/app/main.controller.js
+++ client/app/main.controller.js
@@ -1,23 +1,24 @@
-var angular  = require('angular');
-
 // I am just creating a named function that will be exported below.
 // You don't need $scope now that you are using controllerAs syntax
-var controller = function() {
+function Main() {
     //  Anything attached to this will be attached to the scope and usable in the html file
     // You can alias this like you are doing, or just use this.title and this.name
-    var vm = this;
-    vm.title = 'Main';
-    vm.name = "Kurt"
+  var vm = this;
+  vm.title = 'Main';
+  vm.name = "Kurt";
+  vm.data = 'beans';
 
-    activate();
 
-    ////////////////
+  activate();
 
-    function activate() {
-    
-    }
+  ////////////////
+
+  function activate() {
+  
+  }
+  
 };
 
 
 // Exporting what I want from this file.  It can be a single function or a complex object.
-module.exports = controller;
+module.exports = Main;
