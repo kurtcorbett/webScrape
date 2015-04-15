33f4c1ab047f4da6644223d34d900cd165709fb9
Kurt Corbett <kurtcorbett@gmail.com>
2015-04-15 10:49:14 -0600

    new ads folder with ad.schema, ad.js, and sample ads

--- server/ads/ad.schema.js
+++ server/ads/ad.schema.js
@@ -8,14 +8,26 @@ var adSchema = new Schema({
   description: { type: String, required: true },
   price: Number,
   location: String,
-  img: String,
+  imgUrl: String,
   created_at: Date,
   updated_at: Date
 });
 
+adSchema.pre('save', function(next) {
+  var currentDate = new Date();
+
+  this.updated_at = currentDate;
+
+  if(!this.created_at) {
+    this.created_at = currentDate;
+  }
+
+  next();
+})
+
 // the schema is useless so far
 // we need to create a model using it
-var User = mongoose.model('User', userSchema);
+var Ad = mongoose.model('Ad', adSchema);
 
 // make this available to our users in our Node applications
-module.exports = User;
+module.exports = Ad;
