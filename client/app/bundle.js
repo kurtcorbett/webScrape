864650d87364dbcc7e50d269e3fa108df93a8a1a
Jason Hamm <jasonhamm.me@gmail.com>
2015-04-15 11:08:19 -0600

    Remove wrapper on service

--- client/app/bundle.js
+++ client/app/bundle.js
@@ -95,22 +95,13 @@ module.exports = Main;
 },{}],5:[function(require,module,exports){
 /* @ngInject */
 function classifiedsService($http) {
-    var service = {
-        getClassifieds: getClassifieds
-    };
-    return service;
-
-    ////////////////
-
-    function getClassifieds() {
-      return $http.get('http://localhost:3000/')
-        .success(function(data, status, headers, config) {
-          return data;
-        })
-        .error(function(data, status, headers, config ) {
-          console.log('Error: ' + data);
-        });  
-    };
+    return $http.get('http://localhost:3000/')
+      .success(function(data, status, headers, config) {
+        return data;
+      })
+      .error(function(data, status, headers, config ) {
+        console.log('Error: ' + data);
+      });  
 };
 
 module.exports = classifiedsService;
--- client/app/services/getClassifieds.service.js
+++ client/app/services/getClassifieds.service.js
@@ -1,21 +1,12 @@
 /* @ngInject */
 function classifiedsService($http) {
-    var service = {
-        getClassifieds: getClassifieds
-    };
-    return service;
-
-    ////////////////
-
-    function getClassifieds() {
-      return $http.get('http://localhost:3000/')
-        .success(function(data, status, headers, config) {
-          return data;
-        })
-        .error(function(data, status, headers, config ) {
-          console.log('Error: ' + data);
-        });  
-    };
+    return $http.get('http://localhost:3000/')
+      .success(function(data, status, headers, config) {
+        return data;
+      })
+      .error(function(data, status, headers, config ) {
+        console.log('Error: ' + data);
+      });  
 };
 
 module.exports = classifiedsService;
